da377d1d7fd884efa6f53cc61963eca620720b02
akash sunar <sunarakash1122@gmail.com>
2023-06-02 14:53:37 +0545

    control flow challenge

--- bootcamp-prep-main/pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ bootcamp-prep-main/pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,17 @@
 let name = 'David';
-// let name = 'Nimit';
-// let name = 'Someone else';
+// let name1= 'Nimit';
+// let name2= 'Someone else';
 
 let found;
 
 // YOUR CODE BELOW
+if(name==="David"){
+    found=true;
+}
+else if(name="Nimit"){
+    found=true;
+}
+else if (name==="Someone else")
+{
+    found=false;
+}
--- bootcamp-prep-main/pre-work-workshops/04-control-flow/02-must-be-perfect/must-be-perfect.js
+++ bootcamp-prep-main/pre-work-workshops/04-control-flow/02-must-be-perfect/must-be-perfect.js
@@ -1,9 +1,17 @@
-let word = 'carrots';
-// let word = 'pacific';
+// let word = 'carrots';
+let word = 'pacific';
 // let word = 'perfecto';
 // let word = 'perfect';
 
 
 let lastWord;
 
+
+
 // YOUR CODE BELOW
+if( word[0]==="p"){
+   console.log( lastWord=word.toUpperCase())
+}
+else {
+    console.log(lastWord=word)
+}
