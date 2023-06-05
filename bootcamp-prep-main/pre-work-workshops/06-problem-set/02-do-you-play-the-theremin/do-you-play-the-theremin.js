f48dd4a48525a71c396009284c6566a110988daa
akash sunar <sunarakash1122@gmail.com>
2023-06-05 14:57:04 +0545

    do you play the game

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function doYouPlayTheTheremin(name)
+{
+    if(name[0]==="s" || name[0]==="S")
+    {
+        return true;
+    }
+    return false;
+
+}
+ console.log(doYouPlayTheTheremin("sushil"));
