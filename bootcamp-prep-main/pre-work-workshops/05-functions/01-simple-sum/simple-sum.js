f0936c87ecc43b42e5c5b9dbd2e07b2ec8bef0ea
akash sunar <sunarakash1122@gmail.com>
2023-06-05 12:05:36 +0545

    default greet

--- bootcamp-prep-main/pre-work-workshops/05-functions/01-simple-sum/simple-sum.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/01-simple-sum/simple-sum.js
@@ -1,1 +1,6 @@
 // YOUR CODE BELOW
+function simpleSum(num1,num2){
+    sum=num1+num2;
+    return  sum;
+}
+console.log(simpleSum(4,5));
--- bootcamp-prep-main/pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,7 @@
 // YOUR CODE BELOW
+function defaultGreet(fName,lName="doe")
+{
+    return "Hi "+fName+" "+lName+ "!"
+}
+console.log(defaultGreet("Akash","sunar"))
+console.log(defaultGreet("sushil"))
