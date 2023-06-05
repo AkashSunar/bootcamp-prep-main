94c30da4892b2ceb05b974193f996710d70e42d1
akash sunar <sunarakash1122@gmail.com>
2023-06-05 15:02:18 +0545

    last character

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function lastCharacter (word1,word2)
+{
+if(word1[word1.length-1]===word2[word2.length-1])
+{
+    return true;
+}
+return false;
+}
+console.log(lastCharacter("apples","pumkins"));
