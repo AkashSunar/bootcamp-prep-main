664dd859c98a7895701c68f600b0496d43db9cb2
akash sunar <sunarakash1122@gmail.com>
2023-06-05 13:18:05 +0545

    tax calculator

--- bootcamp-prep-main/pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function taxCalculator(price,state)
+{
+    if(state==="NY"){
+        let totalPrice=price+((4/100)*price)
+        return totalPrice;
+    }
+    else if(state==="NJ"){
+        totalPrice=price+((6.625/100)*price)
+        return totalPrice;
+    }
+}
+console.log(taxCalculator(100,"NY"))
+console.log( taxCalculator(100,"NJ"))
